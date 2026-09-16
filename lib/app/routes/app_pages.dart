8d8a7a8c63cdf17755a176a59fad3818e50d1a4d
ANGGORO RAVI <whyuravi.2008@gmail.com>
2026-09-16 09:43:18 +0700

    feat: setup auth middleware and protect routes

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -4,6 +4,7 @@ import '../views/login_view.dart';
 import '../views/product_list_view.dart';
 import '../views/product_detail_view.dart';
 import '../views/favorites_view.dart';
+import 'auth_middleware.dart';
 
 part 'app_routes.dart';
 
@@ -16,17 +17,17 @@ class AppPages {
     GetPage(
       name: _Paths.LOGIN,
       page: () => const LoginView(),
-      transition: Transition.fadeIn, // Custom transition 1
+      transition: Transition.fadeIn,
     ),
     GetPage(
       name: _Paths.PRODUCTS,
       page: () => const ProductListView(),
-      // Nested routes inside PRODUCTS
+      middlewares: [AuthMiddleware()],
       children: [
         GetPage(
           name: _Paths.PRODUCT_DETAIL,
           page: () => const ProductDetailView(),
-          transition: Transition.zoom, // Custom transition 2
+          transition: Transition.zoom,
         ),
         GetPage(
           name: _Paths.FAVORITES,
